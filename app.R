9ea8018349ba4f5f0989a8f7d9d0a2460ddd7d08
JulDenereaz <denereaz.julien@gmail.com>
2022-03-31 15:55:07 +0200

    version 0.1

--- app.R
+++ app.R
@@ -1,49 +1,108 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(growthcurver)
+library(shinydashboard)
+library(shinyWidgets)
+library(shiny) 
+library(readxl) 
+library(tools)
+library(stringr) 
+library(ggplot2)
+library(rhandsontable) 
+library(reshape2) 
+library(dplyr)
+library(lemon) 
+library(scales) 
+library(esquisse) 
+library(gridExtra)
+library(ggpubr)
+library(parallel)
+library(Cairo)
+library(sortable)
+library(shinythemes)
+library(RColorBrewer)
+source('utility.R', local = TRUE)
 
-library(shiny)
+version <- "1.0"
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+##### UI #####
+ui = dashboardPage(
+  dashboardHeader(title = "TECAN Extractor"),
+  dashboardSidebar(
+    fileInput("files", "Choose TECAN Excel File",
+              multiple = TRUE,
+              accept = c(".xlsx", ".txt")),
+    
+    div(style="height:calc(100vh - 250px);",
+        width=12,
+        uiOutput("multifiles_options"),
+        uiOutput("options"),
+        uiOutput("downloads"),
+    ),
+    tags$hr(style='margin-bottom:5px'),
+    column(
+      width=12,
+      align="center", 
+      p('\u00A9 Julien D\u00E9n\u00E9r\u00E9az'),
+      p(paste0('2021 - Version ', version))
     )
+    
+  ),
+  dashboardBody(
+    tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+    tags$style(HTML('.shiny-split-layout>div {overflow: visible;}')),
+    fluidRow(
+      column(
+        width=12,
+        box(
+          height='calc(calc(100vh - 140px)/2)',
+          width=2,
+          title="Normalized data table Preview",
+          status = 'primary',
+          rHandsontableOutput("table_preview", height = 'calc(calc(100vh - 280px)/2)')
+        ),
+        box(
+          height='calc(calc(100vh - 140px)/2)',
+          width=5,
+          title="Group design",
+          status = 'primary',
+          rHandsontableOutput("groups", height = 'calc(calc(100vh - 280px)/2)')
+        ),
+        box(
+          width=5,
+          height='calc(calc(100vh - 140px)/2)',
+          title="Plot Options and Theme",
+          status = 'primary',
+          tags$style(HTML(".box {overflow-y:auto}")),
+          uiOutput("graph_options")
+        )
+      ),
+      column(
+        width=12,
+        tabBox(
+          width=12,
+          title="Plots",
+          height = 'calc(calc(100vh - 110px)/2)',
+          tabPanel(
+            'Growth Plot', 
+            plotOutput('plot')
+          ),
+          tabPanel(
+            'AUC Plot',
+            column(
+              width=2,
+              uiOutput("gro_opt")
+            ),
+            
+            column(
+              width=5,
+              plotOutput('bar_graph')              
+            ),
+            column(
+              width=5,
+              plotOutput('tile_graph')
+            )
+          )
+        )
+      )
+    )
+  )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
